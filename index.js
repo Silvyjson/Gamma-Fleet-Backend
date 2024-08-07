e5b5447931cd4b820bb662a34ad4196a289a625e
Silvyjson <139075245+Silvyjson@users.noreply.github.com>
2024-08-07 13:08:41 +0100

    feat: made some changes

--- index.js
+++ index.js
@@ -15,10 +15,10 @@ const app = express();
 
 app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
-// app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(
   cors({
     origin: [
+      "http://localhost:5173",
       "https://silvyjson.github.io",
       "https://silvyjson.github.io/Gamma-Fleet/",
     ],
